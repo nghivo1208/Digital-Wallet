581bc44bae85d9707c4556f7fd52a507bdb12b46
nghivo1208 <66132526+nghivo1208@users.noreply.github.com>
2022-01-21 21:31:39 +0700

    feature/implement_coingecko_api (#2)
    * implement Coingecko api
    
    * implement CoinGecko api

---
+++ app/src/main/java/com/example/digitalwallet/network2/model/response/CoinGeckoResponse.kt
@@ -0,0 +1,13 @@
+package com.example.digitalwallet.network2.model.response
+
+import com.squareup.moshi.Json
+import com.squareup.moshi.JsonClass
+
+@JsonClass(generateAdapter = true)
+data class NewCurrencyResponse(
+    @Json(name = "name") val name: String?,
+    @Json(name = "symbol") val symbol: String?,
+    @Json(name = "current_price") val currentPrice: Double?,
+    @Json(name = "image") val image: String?,
+    @Json(name = "market_cap_rank") val marketCapRank: String?
+)
