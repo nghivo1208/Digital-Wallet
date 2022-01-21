7ebb7aec640d130a9c684a674a8132a2ea9badfa
NghiVo <nghivo1208@gmail.com>
2022-01-21 12:59:50 +0700

    implement CoinGecko api

--- app/src/main/java/com/example/digitalwallet/network2/NetworkFactory.kt
+++ app/src/main/java/com/example/digitalwallet/network2/NetworkFactory.kt
@@ -1,6 +1,7 @@
 package com.example.digitalwallet.network2
 
 import com.example.digitalwallet.network.service.CurrencyService
+import com.example.digitalwallet.network2.service.CoinGeckoService
 import com.squareup.moshi.Moshi
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
@@ -12,8 +13,8 @@ object NetworkFactory {
 
     private const val HTTP_TIME_OUT = 15L
 
-    fun createDriverService(retrofit: Retrofit): CurrencyService =
-        retrofit.create(CurrencyService::class.java)
+    fun createDriverService(retrofit: Retrofit): CoinGeckoService =
+        retrofit.create(CoinGeckoService::class.java)
 
     fun createRetrofit(
         okHttpClient: OkHttpClient,
--- app/src/main/java/com/example/digitalwallet/network2/service/CoinGeckoService.kt
+++ app/src/main/java/com/example/digitalwallet/network2/service/CoinGeckoService.kt
@@ -1,17 +1,18 @@
 package com.example.digitalwallet.network2.service
 
-import com.example.digitalwallet.network2.model.response.GetPriceResponse
+import com.example.digitalwallet.network2.model.response.CoinGeckoResponse
 import retrofit2.http.GET
 import retrofit2.http.Query
 
-interface CurrencyService {
+interface CoinGeckoService {
 
         @GET("api/v3/coins/markets")
         suspend fun getListCoin(
             @Query("currency") currency: String,
             @Query("order") order: String,
             @Query("per_page") perPage: Int,
-            @Query("page") page: Int
-        ): List<GetPriceResponse>?
+            @Query("page") page: Int,
+            @Query("sparkline") SparkLine: Boolean
+        ): List<CoinGeckoResponse>?
 
 }
