5d72a663b3883447ce9eb634057058b5810c99d8
nghivo1208 <66132526+nghivo1208@users.noreply.github.com>
2022-01-23 10:44:10 +0700

    Implement room (#3)
    * Implement room
    
    * Implement room
    
    * Implement room

---
+++ app/src/main/java/com/example/digitalwallet/local2/room/MainDatabase.kt
@@ -0,0 +1,18 @@
+package com.example.digitalwallet.local2.room
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import com.example.digitalwallet.local.dao.CurrencyDao
+import com.example.digitalwallet.local.model.Currency
+import com.example.digitalwallet.local2.dao.CoinGeckoCurrencyDao
+import com.example.digitalwallet.local2.model.CoinGeckoCurrency
+
+@Database(
+    entities = [Currency::class, CoinGeckoCurrency::class],
+    version = 1,
+    exportSchema = true
+)
+abstract class MainDatabase : RoomDatabase() {
+    abstract fun currencyDao(): CurrencyDao
+    abstract fun coinGeckoCurrencyDao(): CoinGeckoCurrencyDao
+}
